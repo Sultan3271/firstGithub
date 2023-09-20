0807bd23d587cffd6518de08aebe8187767b497d
sultan khan <97776939+Sultan3271@users.noreply.github.com>
2023-09-19 20:44:17 -0700

    Update TabNavigator.tsx

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,48 +1,66 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import {Image} from 'react-native'
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
+import Friends from '../screens/Friends';
 import UserProfile from '../screens/UserProfile';
-import { styles } from '../styles/Styles';
-import { useIsFocused } from '@react-navigation/native';
-const Tab = createMaterialBottomTabNavigator();
+
+const Tab = createBottomTabNavigator();
+
+const data = [
+  { key: 'Home', iconUrl: 'https://cdn-icons-png.flaticon.com/128/1946/1946436.png' },
+  { key: 'Friends', iconUrl: 'https://cdn-icons-png.flaticon.com/128/8138/8138685.png' },
+  { key: 'UserProfile', iconUrl: 'https://cdn-icons-png.flaticon.com/128/1077/1077114.png' },
+];
+
+const TabBarIcon = ({ iconUrl, focused }) => {
+  return (
+    <Image
+      source={{ uri: iconUrl }}
+      style={{
+        width: 24,
+        height: 24,
+        tintColor: focused ? 'green' : 'gray',
+        marginBottom: -3,
+      }}
+    />
+  );
+};
+
+const renderItem = ({ item }) => {
+  return (
+    <TouchableOpacity style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TabBarIcon iconUrl={item.iconUrl} />
+      <Text style={{ color: 'gray', fontSize: 12, marginTop: 4 }}>{item.key}</Text>
+    </TouchableOpacity>
+  );
+};
 
 function MyTabs() {
-  const isFocused= useIsFocused();
   return (
     <Tab.Navigator
-      initialRouteName="Feed"
-      
-      barStyle={{ backgroundColor: '#fff2f2' }}
-      
+      tabBarOptions={{
+        style: {
+          height: 60,
+          backgroundColor: 'white',
+          borderTopWidth: 0,
+        },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          const item = data.find((d) => d.key === route.name);
+          return <TabBarIcon iconUrl={item.iconUrl} focused={focused} />;
+        },
+      })}
     >
-
-      <Tab.Screen
-        name="Home"
-        component={Home}
-         
-        options={{
-            
-          tabBarLabel:false,
-         
-          tabBarIcon: ()=>(
-            <Image source={{uri:'https://cdn-icons-png.flaticon.com/128/1946/1946436.png'}} style={styles.tabIcon} />
-          )
-        }}
-        
-      />
-      <Tab.Screen
-        name="Profile"
-        component={UserProfile}
-        options={{
-          tabBarLabel:false,
-          tabBarIcon: ()=>(
-            <Image source={{uri:'https://cdn-icons-png.flaticon.com/128/1077/1077114.png'}} style={styles.tabIcon} />
-          )
-
-        }}
-        />
+      <Tab.Screen name="Home" component={Home} options={{headerShown:false, tabBarShowLabel:false}}/>
+      <Tab.Screen name="Friends" component={Friends} options={{headerShown:false, tabBarShowLabel:false}} />
+      <Tab.Screen name="UserProfile" component={UserProfile} options={{headerShown:false, tabBarShowLabel:false}} />
+      
     </Tab.Navigator>
   );
 }
 
-export default MyTabs
+
+
+export default MyTabs;
