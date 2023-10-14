ae7d1b60830169c2da733be1d4a60f320cb4b517
Christian Marcellino <cam179@students.uwf.edu>
2023-10-13 19:59:14 -0500

    Recentered the scholar's banner

--- src/components/ScholarBanner.tsx
+++ src/components/ScholarBanner.tsx
@@ -6,8 +6,7 @@ import  Colors  from '../Theme/ScholarColors';
 export default function ScholarBanner(props: any) {
   const iconColor=Colors.primary
   return (
-    
-    <View style={{ flexDirection: "row", marginBottom: 10, alignItems: 'center', left: 20, }}>
+    <View style={{ flexDirection: "row", marginBottom: 10, alignItems: 'center', justifyContent: "center"}}>
       <Icon name={"school"} size={100} style={{color:Colors.primary}} />
       <Text style={styles.iconHeaderText}>{props.text}</Text>
     </View>
